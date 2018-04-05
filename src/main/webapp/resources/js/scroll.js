245fa1b6bd48a1f4d65ade1a4c5c409c19478f1b
shilpij <shilpij@DEDSHILPIJ.Virtusa.com>
2018-04-05 09:25:59 +0530

    commit

---
+++ src/main/webapp/resources/js/scroll.js
@@ -0,0 +1,31 @@
+var slideIndex = 1;
+$(document).ready(function() {
+	showDivs(slideIndex);
+	setInterval(function() {
+		showDivs(slideIndex += 1);
+	}, 3000);
+	
+});
+
+function plusDivs(n) {
+	showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+	var i;
+	var x = document.getElementsByClassName("mySlides");
+	if (n > x.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = x.length;
+	}
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	x[slideIndex - 1].style.display = "block";
+}
+
+
+
+
